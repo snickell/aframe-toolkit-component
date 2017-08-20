30a469883883f098ea7f5ee69cc1a9ba78e23865
Seth Nickell <snickell@gmail.com>
2017-08-19 19:51:32 -0700

    Don't babelify /lib

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         {
           test: /.js?$/,
           loader: 'babel-loader',     
-          exclude: /node_modules/,
+          exclude: [/node_modules/, "/lib"],
           query: {
             presets: ['env'],
             plugins: [require('babel-plugin-transform-object-rest-spread')]            
