1b8f423461b74edab6401d563789c12df88dd9a1
Seth Nickell <snickell@gmail.com>
2017-08-22 12:36:41 -0700

    Rename dist component

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        "a-toolkit": "./a-toolkit/index.js"        
+        "aframe-toolkit-component": "./a-toolkit/index.js"        
     },
     output: {
         path: path.resolve(__dirname, ""),
